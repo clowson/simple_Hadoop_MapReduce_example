e2dc929cd85a34b602301bf0ed648a9052c2fb82
clowson <clowson@ucsc.edu>
2024-01-26 23:04:32 +0000

    added stopwords

--- mapper.py
+++ mapper.py
@@ -9,6 +9,10 @@ for line in sys.stdin:
     # split the line into words; splits on any whitespace
     words = line.split()
 
+    # set stopwords and punctuation 
+    stopwords = set (['the', 'and', '.', ',', ':', '-', '\"', 'spiders', 'four'])
+
     # output tuples (word, 1) in tab-delimited format
     for word in words:
+       if word not in stopwords:
         print '%s\t%s' % (word, "1")
